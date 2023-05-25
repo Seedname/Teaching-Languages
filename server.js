550ee41f124c909e84cca5f94f249e03e0b9f065
Seedname <45999377+Seedname@users.noreply.github.com>
2023-05-25 09:37:02 -0400

    update footer

--- server.js
+++ server.js
@@ -5,24 +5,23 @@ const fs = require('fs');
 const http = require('http');
 const https = require('https');
 
-const privateKey  = fs.readFileSync('/etc/letsencrypt/live/tucanspeak.ddns.net/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/tucanspeak.ddns.net/fullchain.pem', 'utf8');
-const credentials = {cert: certificate, key: privateKey};
+// const privateKey  = fs.readFileSync('/etc/letsencrypt/live/tucanspeak.ddns.net/privkey.pem', 'utf8');
+// const certificate = fs.readFileSync('/etc/letsencrypt/live/tucanspeak.ddns.net/fullchain.pem', 'utf8');
+// const credentials = {cert: certificate, key: privateKey};
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.listen(80);
-https.createServer(credentials, app).listen(443);
+// https.createServer(credentials, app).listen(443);
 
 app.use(express.static('public'));
 
 const configuration = new Configuration({
     apiKey: '',
 });
-  
-// Serve the HTML file
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -40,11 +39,9 @@ async function getAiResponse(topic) {
     return completion.data.choices[0].text;
 }
 
-// Handle the form POST request and get the response from OpenAI API
 app.post('/ask', async (req, res) => {
     try {
       let searchTerm = req.body.searchTerm;
-      // Update this part
       const response = await getAiResponse(searchTerm);
   
       res.json({ answer: response});
